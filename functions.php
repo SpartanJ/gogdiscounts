db17d6a28652ef2c4fad93317865f14b36d57136
Martín Lucas Golini <spartanj@gmail.com>
2020-12-20 02:09:38 -0300

    Minor fix.

--- functions.php
+++ functions.php
@@ -10,6 +10,7 @@ function get_url( $order_type, $order_dir ) {
 	parse_str( $_SERVER['QUERY_STRING'], $query_arr );
 	$query_arr['order_type'] = $order_type;
 	$query_arr['order_dir'] = $order_dir;
+	$query_arr['location'] = get_var('location');
 	return '?' . http_build_query( $query_arr );
 }
 
